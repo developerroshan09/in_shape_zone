f604c2a99c2d77f3b918b7dfd1f6d960a18845d1
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2025-10-29 15:52:47 +0545

    server

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.use('/', require('./server/routes/foodRoutes'));
 
 // Starting the server and listening on the specified port
 app.listen(PORT, () => {
-    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
