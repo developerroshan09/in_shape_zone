cc973036148d77ad3390b4b5e8c5cd8fb2ef716c
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2025-10-29 15:38:34 +0545

    cloud mongo

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect(process.env.MONGO_URI, {//'mongodb://127.0.0.1:27017/users', { //process.env.MONGO_URI, {
+        const con = await mongoose.connect("mongodb+srv://developerroshan09_db_user:keqgun-wuwpyq-4Qedry@cluster0.rvpxb6b.mongodb.net/?appName=Cluster0", {//'mongodb://127.0.0.1:27017/users', { //process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
