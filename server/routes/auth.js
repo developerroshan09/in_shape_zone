1e6f10d07c08c4300d2031c5a416aaff4c57c7a1
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2025-10-30 12:31:28 +0545

    check

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const route = express.Router();
-const verifyToken = require('./../../middleware/authMiddleware');
+// const verifyToken = require('./../../middleware/authMiddleware');
 const authController = require('./../controller/authController');
 
 // defining apis and their corresponding controller functions
 route.post('/register', authController.register);
 route.post('/login', authController.login);
-route.post("/logout", verifyToken, authController.logout);
+route.post("/logout", authController.logout);
 
 module.exports = route;
